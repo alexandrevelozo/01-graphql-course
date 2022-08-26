ae19a7f7110d25d76b16dc50d938036b5d4c6c13
Alexandre Velozo <alexandrevelozo4@gmail.com>
2022-08-26 15:08:55 -0300

    Preparing environment for next section

--- src/graphql/post/resolvers.js
+++ src/graphql/post/resolvers.js
@@ -5,51 +5,13 @@ const posts = async (_, { input }, { getPosts }) => {
 };
 
 const post = async (_, { id }, { getPosts }) => {
-  const response = await getPosts('/' + id);
-  const post = await response.json();
-
-  if (Math.random() > 0.5) {
-    return {
-      statusCode: 500,
-      message: 'Post timeout',
-      timeout: 123,
-    };
-  }
-
-  if (typeof post.id === 'undefined') {
-    return {
-      statusCode: 404,
-      message: 'Post not found',
-      postId: id,
-    };
-  }
-  return post;
+  const post = await getPosts('/' + id);
+  return post.json();
 };
 
 export const postResolvers = {
   Query: {
     post,
     posts,
-  },
-  Post: {
-    unixTimestamp: () => {
-      const date = new Date().getTime() / 1000;
-      return Math.floor(date);
-    },
-  },
-  PostResult: {
-    __resolveType: (obj) => {
-      if (typeof obj.postId !== 'undefined') return 'PostNotFoundError';
-      if (typeof obj.timeout !== 'undefined') return 'PostTimeoutError';
-      if (typeof obj.id !== 'undefined') return 'Post';
-      return null;
-    },
-  },
-  PostError: {
-    __resolveType: (obj) => {
-      if (typeof obj.postId !== 'undefined') return 'PostNotFoundError';
-      if (typeof obj.timeout !== 'undefined') return 'PostTimeoutError';
-      return null;
-    },
   },
 };
